3c451a970d32a66dec2ad3e93111ee4fcad2e063
hndmin <andysjhan2@naver.com>
2022-08-01 18:23:34 +0900

    doing

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function focusOff(){
     document.getElementById('profile').classList.remove('focusOut');
     document.getElementById('list').classList.remove('focusIn');
 }
+
+function showFullImg(){
+    document.getElementsByClassName('showFullImg')[0].classList.remove('hide');
+    document.getElementsByClassName('showFullImg')[0].classList.add('show');
+}
+function closeFullImg(){
+    console.log("aha!");
+    document.getElementsByClassName('showFullImg')[0].classList.remove('show');
+    document.getElementsByClassName('showFullImg')[0].classList.add('hide');
+    console.log("aha!2");
+}
